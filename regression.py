cbc4251a67b8001bae850958cfcf667b5c28d9fe
Mykaella98 <mykaella.sr@outlook.com>
2025-02-24 11:54:04 +0100

    update regression.py
    added the csv

--- regression.py
+++ regression.py
@@ -1,5 +1,5 @@
 # --- IMPORT SECTION ---
-import pandas as pd # for DataFrames
+import pandas as pd #for DataFrames
 import numpy as np # for numpy array operations
 import matplotlib.pyplot as plt # for visualization
 # importing all the needed functions from scikit-learn
@@ -7,8 +7,12 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import PolynomialFeatures, StandardScaler
 from sklearn.linear_model import LinearRegression, Ridge
 from sklearn.metrics import mean_squared_error
+
 # --- END OF IMPORT SECTION ---
 
 # --- MAIN CODE ---
+path_to_data = "Salary_Data.csv"
+data = pd.read_csv(path_to_data)
+print(data.head())
 
 # --- END OF MAIN CODE ---
